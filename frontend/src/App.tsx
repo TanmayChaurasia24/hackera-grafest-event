2d8be1a49d3aae8fc445f7570e55b2a7a116f909
tanmay <tanmaykumar.kanpur@gmail.com>
2025-04-29 21:54:54 +0530

    ..

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 
@@ -16,7 +15,6 @@ import NotFound from "./pages/NotFound";
 import NavBar from "./components/NavBar";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 
-const queryClient = new QueryClient();
 
 const AppContent = () => {
   return (
@@ -57,7 +55,6 @@ const AppContent = () => {
 };
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <AuthProvider>
         <TooltipProvider>
@@ -67,7 +64,6 @@ const App = () => (
         </TooltipProvider>
       </AuthProvider>
     </BrowserRouter>
-  </QueryClientProvider>
 );
 
 export default App;
